604764f72e08c5e7c0ee063396ce51a154a02779
Prithviraj More <143012269+Prithxvhie44@users.noreply.github.com>
2024-12-22 20:37:25 +0530

    Drum addition

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -3,6 +3,55 @@
 
 var total= document.querySelectorAll(".drum").length;
 for(i=0; i<total; i++){
-document.querySelectorAll("button")[i].addEventListener("click",function(){alert("I got clicked!")})
+
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+    var buttonInner= this.innerHTML;
+    
+    switch(buttonInner){
+        case "w" :
+            var audio= new Audio("sounds/tom-1.mp3");
+            audio.play();
+        break;
+
+        case "a" :
+            var audio= new Audio("sounds/tom-2.mp3");
+            audio.play();
+        break;
+
+        case "s" :
+            var audio= new Audio("sounds/tom-3.mp3");
+            audio.play();
+        break;
+
+        case "d" :
+            var audio= new Audio("sounds/tom-4.mp3");
+            audio.play();
+        break;
+
+        case "j" :
+            var audio= new Audio("sounds/snare.mp3");
+            audio.play();
+        break;
+
+        case "k" :
+            var audio= new Audio("sounds/crash.mp3");
+            audio.play();
+        break;
+
+        case "l" :
+            var audio= new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        break;
+
+
+    }
+        
+
+})
+
 
 }//All buttons display when clicked
+
+
+

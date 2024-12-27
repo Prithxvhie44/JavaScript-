09e0c755337ba2769f0bbd161575d20bd51422b1
Prithviraj More <143012269+Prithxvhie44@users.noreply.github.com>
2024-12-27 11:58:24 +0530

    updation

---
+++ Drum+Kit+Starting+Files/jQuery/index.js
@@ -0,0 +1,25 @@
+
+
+$("h1").addClass('big-title margin');
+
+// $("h1").text("Bye");
+// $("button").text("Don't click me");
+
+// $("img").attr("src");
+// $("a").attr("href", "https://www.yahoo.com");
+
+$("h1").click(function() {
+    $("h1").css("color", "black");
+    });
+
+$("button").click(function() {
+    $("h1").css("color","blue")});
+
+$(document).keypress(function(event){
+    $("h1").text(event.key);
+    key = event.key;
+});
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "red");
+});
